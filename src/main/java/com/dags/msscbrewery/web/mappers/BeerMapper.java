21480049770716ab3267aadab6079aa9536f8d01
Chris <christopher-dags@hotmail.com>
2021-03-24 16:17:29 -0400

    Created DateMapper, used by BeerMapper

--- src/main/java/com/dags/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/com/dags/msscbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import com.dags.msscbrewery.domain.Beer;
 import com.dags.msscbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
     BeerDto beerToBeerDto(Beer beer);
     Beer beerDtoToBeer(BeerDto beerDto);
